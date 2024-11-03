1a7a2a853e46d804b9106be3712c282d35f8ccc8
ashesh-mondal <asheshmondal84@gmail.com>
2024-11-03 10:35:28 +0530

    Listing Model created and sample data added in db for checking purpose

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const PORT = 8080;
 const mongoose = require("mongoose");
+const Listing = require("./models/listing.js");
 
 const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
 
@@ -17,6 +18,23 @@ main()
     console.log(err);
   });
 
-app.listen(() => {
+app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
+});
+
+app.get("/", (req, res) => {
+  res.send("Hi! I am Root");
+});
+
+app.get("/testListing", async (req, res) => {
+  let sampleListing = new Listing({
+    title: "My New Villa",
+    description: "By the beach",
+    price: 1200,
+    location: "Calangute, Goa",
+    country: "India",
+  });
+  await sampleListing.save();
+  console.log("saved");
+  res.send("successful testing");
 });
