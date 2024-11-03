77718bf1a70b2368a792989a7885923305be1c50
ashesh-mondal <asheshmondal84@gmail.com>
2024-11-03 12:11:41 +0530

    Index Route

--- app.js
+++ app.js
@@ -3,6 +3,10 @@ const app = express();
 const PORT = 8080;
 const mongoose = require("mongoose");
 const Listing = require("./models/listing.js");
+const path = require("path");
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
 
 const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
 
@@ -26,15 +30,22 @@ app.get("/", (req, res) => {
   res.send("Hi! I am Root");
 });
 
-app.get("/testListing", async (req, res) => {
-  let sampleListing = new Listing({
-    title: "My New Villa",
-    description: "By the beach",
-    price: 1200,
-    location: "Calangute, Goa",
-    country: "India",
-  });
-  await sampleListing.save();
-  console.log("saved");
-  res.send("successful testing");
+// app.get("/testListing", async (req, res) => {
+//   let sampleListing = new Listing({
+//     title: "My New Villa",
+//     description: "By the beach",
+//     price: 1200,
+//     location: "Calangute, Goa",
+//     country: "India",
+//   });
+//   await sampleListing.save();
+//   console.log("saved");
+//   res.send("successful testing");
+// });
+
+// INDEX ROUTE
+
+app.get("/listings", async (req, res) => {
+  let allListings = await Listing.find({});
+  res.render("listings/index.ejs", { allListings });
 });
