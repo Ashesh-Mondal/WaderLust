0c4ad3a0c5e2807c2b65d799e0d640d5cce8d2e6
ashesh-mondal <asheshmondal84@gmail.com>
2024-11-22 09:46:47 +0530

    Registering (Saving) users in the database

--- app.js
+++ app.js
@@ -78,15 +78,15 @@ app.use((req, res, next) => {
   next();
 });
 
-app.get("/demouser", async (req, res) => {
-  const fakeUser = new User({
-    email: "student@gmail.com",
-    username: "delta-student",
-  });
+// app.get("/demouser", async (req, res) => {
+//   const fakeUser = new User({
+//     email: "student@gmail.com",
+//     username: "delta-student",
+//   });
 
-  let registeredUser = await User.register(fakeUser, "helloworld");
-  res.send(registeredUser);
-});
+//   let registeredUser = await User.register(fakeUser, "helloworld");
+//   res.send(registeredUser);
+// });
 
 app.use("/listings", listingRouter);
 app.use("/listings/:id/reviews", reviewRouter);
--- routes/user.js
+++ routes/user.js
@@ -1,11 +1,30 @@
 const express = require("express");
 const router = express.Router();
-const User = require("../models/user");
+const User = require("../models/user.js");
 const wrapAsync = require("../utils/wrapAsync");
 
 // SingnUp
 router.get("/signup", (req, res) => {
   res.render("users/signup.ejs");
 });
+
+// Register User in DB
+router.post(
+  "/signup",
+  wrapAsync(async (req, res) => {
+    try {
+      let { username, email, password } = req.body;
+      const newUser = new User({ username, email });
+      const registeredUser = await User.register(newUser, password);    // Used to save data in DB and hash password
+      console.log(registeredUser);
+      req.flash("success", "Welcome to Wanderlust!");
+      res.redirect("/listings");
+    } catch (e) {
+      console.log(e);
+      req.flash("error", e.message);
+      res.redirect("/signup");
+    }
+  })
+);
 
 module.exports = router;
