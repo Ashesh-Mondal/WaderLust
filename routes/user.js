694a29441579fefac130ea6b77b4aef2f30bd16b
ashesh-mondal <asheshmondal84@gmail.com>
2024-11-22 10:01:47 +0530

    Authenticating and logging in users

--- routes/user.js
+++ routes/user.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 const User = require("../models/user.js");
 const wrapAsync = require("../utils/wrapAsync");
+const passport = require("passport");
 
 // SingnUp
 router.get("/signup", (req, res) => {
@@ -15,7 +16,7 @@ router.post(
     try {
       let { username, email, password } = req.body;
       const newUser = new User({ username, email });
-      const registeredUser = await User.register(newUser, password);    // Used to save data in DB and hash password
+      const registeredUser = await User.register(newUser, password); // Used to save data in DB and hash password
       console.log(registeredUser);
       req.flash("success", "Welcome to Wanderlust!");
       res.redirect("/listings");
@@ -25,6 +26,24 @@ router.post(
       res.redirect("/signup");
     }
   })
+);
+
+// Login Form
+router.get("/login", (req, res) => {
+  res.render("users/login.ejs");
+});
+
+// Login User
+router.post(
+  "/login",
+  passport.authenticate("local", {
+    failureRedirect: "/login",
+    failureFlash: true,
+  }),
+  async (req, res) => {
+    req.flash("success", "Welcome back to Wanderlust!");
+    res.redirect("/listings");
+  }
 );
 
 module.exports = router;
