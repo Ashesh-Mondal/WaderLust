82cabaaac4d595287e3ddbd47b9a77e3989af0ad
ashesh-mondal <asheshmondal84@gmail.com>
2024-12-05 09:46:34 +0530

    Using router.route() in our project to avoid duplicate route naming and thus typing errors

--- routes/listing.js
+++ routes/listing.js
@@ -1,58 +1,41 @@
 const express = require("express");
 const router = express.Router();
-const Listing = require("../models/listing");
 const wrapAsync = require("../utils/wrapAsync");
 const { isLoggedIn, isOwner, validateListing } = require("../middleware.js");
 
 const listingController = require("../controllers/listing");
 
-// INDEX ROUTE
-
-router.get("/", wrapAsync(listingController.index));
+// INDEX ROUTE && CREATE ROUTE
+router
+  .route("/")
+  .get(wrapAsync(listingController.index))
+  .post(
+    isLoggedIn,
+    validateListing,
+    wrapAsync(listingController.createListing)
+  );
 
 // NEW ROUTE
-
 router.get("/new", isLoggedIn, listingController.renderNewForm);
 
-// CREATE ROUTE
-
-router.post(
-  "/",
-  isLoggedIn,
-  validateListing,
-  wrapAsync(listingController.createListing)
-);
-
-// SHOW ROUTE
-
-router.get("/:id", wrapAsync(listingController.showListing));
+// SHOW ROUTE && UPDATE ROUTE && DELETE ROUTE
+router
+  .route("/:id")
+  .get(wrapAsync(listingController.showListing))
+  .put(
+    isLoggedIn,
+    isOwner,
+    validateListing,
+    wrapAsync(listingController.updateListing)
+  )
+  .delete(isLoggedIn, isOwner, wrapAsync(listingController.destroyListing));
 
 // EDIT ROUTE
-
 router.get(
   "/:id/edit",
   isLoggedIn,
   isOwner,
   wrapAsync(listingController.renderEditForm)
-);
-
-// UPDATE ROUTE
-
-router.put(
-  "/:id",
-  isLoggedIn,
-  isOwner,
-  validateListing,
-  wrapAsync(listingController.updateListing)
-);
-
-// DELETE ROUTE
-
-router.delete(
-  "/:id",
-  isLoggedIn,
-  isOwner,
-  wrapAsync(listingController.destroyListing)
 );
 
 module.exports = router;
--- routes/review.js
+++ routes/review.js
@@ -1,7 +1,5 @@
 const express = require("express");
 const router = express.Router({ mergeParams: true });
-const Listing = require("../models/listing");
-const Review = require("../models/review");
 const wrapAsync = require("../utils/wrapAsync");
 const {
   validateReview,
