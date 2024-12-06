3138990d35c4129a22c4dc1ee6a4a8099abe42fb
ashesh-mondal <asheshmondal84@gmail.com>
2024-12-06 20:27:31 +0530

    Storing files in the cloud

--- routes/listing.js
+++ routes/listing.js
@@ -6,7 +6,8 @@ const { isLoggedIn, isOwner, validateListing } = require("../middleware.js");
 const listingController = require("../controllers/listing");
 
 const multer = require("multer");
-const upload = multer({ dest: "uploads/" });
+const { storage } = require("../cloudConfig.js");
+const upload = multer({ storage });
 
 // INDEX ROUTE && CREATE ROUTE
 router
