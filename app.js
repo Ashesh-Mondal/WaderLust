e4f339d7a27d771ad98e7ac352dcfe782c4bf79d
ashesh-mondal <asheshmondal84@gmail.com>
2024-12-11 20:40:50 +0530

    Storing session related data in mongoDb store

--- app.js
+++ app.js
@@ -11,6 +11,7 @@ const methodOverride = require("method-override");
 const ejsMate = require("ejs-mate");
 const ExpressError = require("./utils/ExpressError.js");
 const session = require("express-session");
+const MongoStore = require("connect-mongo");
 const flash = require("connect-flash");
 const User = require("./models/user.js");
 const passport = require("passport");
@@ -32,7 +33,23 @@ app.engine("ejs", ejsMate);
 
 app.use(express.static(path.join(__dirname, "/public")));
 
+// const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
+const dbUrl = process.env.ATLASDB_URL;
+
+const store = MongoStore.create({
+  mongoUrl: dbUrl,
+  crypto: {
+    secret: "supersecretcode",
+  },
+  touchAfter: 24 * 60 * 60,
+});
+
+store.on("error", () => {
+  console.log("Session store error", err);
+});
+
 const sessionOptions = {
+  store,
   secret: "supersecretcode",
   resave: false,
   saveUninitialized: true,
@@ -53,9 +70,6 @@ passport.use(new LocalStrategy(User.authenticate()));
 
 passport.serializeUser(User.serializeUser()); // Used to save the creadentials of a user for a complete session
 passport.deserializeUser(User.deserializeUser()); // Used to delete the creadentials of a user after the session is completed
-
-// const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
-const dbUrl = process.env.ATLASDB_URL;
 
 async function main() {
   await mongoose.connect(dbUrl);
